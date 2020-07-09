3ed1935cc1aae9f68d1dc02ea95c6d796281eb54
SpideyFanDan <dsmith.teach@gmail.com>
2020-07-09 14:23:46 -0600

    completed adding x or o to boxes as part of gamePlay function

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let currentPlayer = 'X';
-let gameboard;
+let gameBoard = 0;
+let gameScore = 0;
 
+let boxes = document.querySelectorAll('.box');
+boxes.forEach((box) => {
+	box.addEventListener('click', gamePlay);
+});
 function switchPlayers() {
 	if (currentPlayer === 'X') {
 		currentPlayer = 'O';
@@ -10,4 +15,52 @@ function switchPlayers() {
 	/* ternary operator code
   currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
 */
+}
+
+function checkWinCondition() {
+	// checks for win conditions
+}
+function checkTieCondition() {
+	// check if there is a tie
+}
+function checkBoxNotOccupied(event) {
+	if ((event.target.innerText = null)) {
+		turnSequence();
+	}
+
+	// determine if the game box that is clicked is empty!
+}
+
+// function startGame() {
+// 	gameScore = 0;
+// 	tieScore = 0;
+// 	for (let i = 0; i < boardSquare.length; i++) {
+// 		boardSquare[i].dataset.used = 0;
+// 		boardSquare[i].innerHTML = '';
+// 		boardSquare[i].classList.remove('clicked');
+// 	}
+// }
+// board.addEventListener('click', placePiece);
+// function placePiece(event) {
+// 	if (event.target.dataset.used === '0') {
+// 		const tic = document.createElement('p');
+// 		tic.innerText = currentPlayer;
+// 		tic.setAttribute('class', 'tic');
+// 		event.target.appendChild(tic);
+// 		event.target.dataset.used = 1;
+// 		event.target.classList.add('clicked');
+// 		tieScore += 1;
+//     }
+//     startGame();
+//     switchPlayers();
+// }
+function turnSequence(event) {
+	event.target.innerText = currentPlayer;
+	//  all the things that happen when a user takes a turn
+	// 1.
+	// 2.
+	switchPlayers();
+}
+function writeMessage(message) {
+	// update the gameboard message with the message passed to it
 }
